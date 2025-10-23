b79605c439951619df8f5fc51c76880c2a929fd0
raji-ayyub <ayyubraji87@gmail.com>
2025-10-23 15:48:36 +0100

    update

--- database/database.py
+++ database/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from pymysql.constants import CLIENT
 import os
 
 load_dotenv()
@@ -11,14 +12,56 @@ load_dotenv()
 db_url = f"mysql+pymysql://{os.getenv("dbuser")}:{os.getenv("dbpassword")}@{os.getenv("dbhost")}:{os.getenv("dbport")}/{os.getenv("dbname")}"
 
 
-engine = create_engine(db_url)
+engine = create_engine(
+    db_url,
+    connect_args={"client_flag": CLIENT.MULTI_STATEMENTS}
+    )
 
-session = sessionmaker(bind=engine)
+session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 db = session()
 
-query = text("select * from user")
+# query = text("select * from user")
 
-users = db.execute(query).fetchall()
+# users = db.execute(query).fetchall()
 
-print(users)
+# print(users)
+
+
+create_user =  text("""
+    CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            password VARCHAR(100) NOT NULL
+        );      
+""")
+
+
+# create_courses =  text("""
+#     CREATE TABLE IF NOT EXISTS users (
+#             id INT AUTO_INCREMENT PRIMARY KEY,
+#             title VARCHAR(100) NOT NULL,
+#             level VARCHAR(100) NOT NULL,
+#             password VARCHAR(100) NOT NULL
+#         );      
+
+
+# """)
+
+# create_enrollment =  text("""
+#     CREATE TABLE IF NOT EXISTS users (
+#             id INT AUTO_INCREMENT PRIMARY KEY,
+#             name VARCHAR(100) NOT NULL,
+#             email VARCHAR(100) NOT NULL,
+#             password VARCHAR(100) NOT NULL
+#         );      
+
+
+# """)
+
+db.execute(create_user)
+# db.execute(create_courses)
+# db.execute(create_enrollment)
+
+
